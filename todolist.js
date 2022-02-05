8b18ae8d52a63f12b7696023d25e739b523588dc
Valiantsin Dashkevich <v6087573@gmail.com>
2022-02-05 15:13:43 +0300

    added files

--- todolist.js
+++ todolist.js
@@ -1,70 +1,110 @@
+
 const form = document.querySelector("#form")
 const todo = document.querySelector("#todo")
 const inputText = document.querySelector("#inputText");
 const btnSubmit = document.querySelector("#btnSubmit");
 const btnClearAll = document.querySelector("#btnClearAll");
+const modal = document.querySelector("#modal");
+
+const addCard = () => {
+    todo.innerHTML = '';
+
+    data.forEach((task) => {
+        todo.innerHTML += `
+        <div class='${task.done ? 'card carddash'  : 'card'}' id=${task.id}>
+        <span class="title">${task.title}</span> 
+        <div class = "cardButtons">
+        <button class='btnDelete'>Delete</button>
+        <button class='btnDone'>DONE</button>
+        <button class='btnEdit'>Edit</button>
+        </div>
+        </div>
+        `;   
+    })
+}
+
+
+
 
 let data = [];
 
 btnSubmit.addEventListener("click", (event) => {
     event.preventDefault();
 
-        data.push(inputText.value);
+        data.push({title: inputText.value, done: false, id: Date.now()});
 
-        todo.innerHTML = '';
-
-        data.forEach((title, index) => {
-            todo.innerHTML += `
-            <div class='card' id=${index}>
-            ${title}
-            <button id='btnDelete'>Delete</button>
-            <button id='btnDone'>DONE</button>
-            <button id='btnEdit'>Edit</button>
-            </div>
-            `;   
-        })
+    addCard();
  
     form.reset();
 });
 
+btnClearAll.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    data = [];
+    todo.innerHTML = "";
+});
+
 todo.addEventListener('click', (event) => {
-    if(event.target.id === 'btnDelete') {
+    if(event.target.classList.contains("btnDelete")) {
         const card = event.target.closest('.card');
         const cardId = +card.id;
 
-        data.splice(cardId, 1)
-
-        todo.innerHTML = '';
+        const cardIndexInData = data.findIndex((task) => task.id === cardId);
         
-        data.forEach((title, index) => {
-            todo.innerHTML += `
-            <div class='card' id=${index}>
-            ${title}
-            <button id='btnDelete'>Delete</button>
-            <button id='btnDone'>DONE</button>
-            <button id='btnEdit'>Edit</button>
-            </div>
-            `;   
-        })
+        data.splice(cardIndexInData, 1);
+
+        addCard();
     }
 });
 
 todo.addEventListener('click', (event) => {
-    if(event.target.id === 'btnDone') {
+    if(event.target.classList.contains("btnDone")) {
         const card = event.target.closest('.card');
         const cardId = +card.id;
-
-        todo.innerHTML = '';
+        const cardIndexInData = data.findIndex((task) => task.id === cardId);
         
-        data.forEach((title, index) => {
-            todo.innerHTML += `
-            <div class=`${index === cardId ? 'card dash'  : 'card'}` id=${index}>
-            <strike>${title}</strike>
-            <button id='btnDelete'>Delete</button>
-            <button id='btnDone'>DONE</button>
-            <button id='btnEdit'>Edit</button>
-            </div>
-            `;   
-        })
+        data[cardIndexInData].done = !data[cardIndexInData].done;
+
+        addCard();
+    }
+
+        if(event.target.classList.contains("btnEdit")) {
+            modal.classList.add("visible");
+    }
+});
+
+todo.addEventListener('click', (event) => {
+    if(event.target.classList.contains("btnEdit")) {
+        const card = event.target.closest('.card');
+        const title = card.querySelector(".title");
+        const info = title.textContent;
+        const inputmodal = modal.querySelector(".inputTextModal");
+        inputmodal.value = info;
+        inputmodal.id = card.id;
+        modal.classList.add("visible");
+   }
+});
+
+modal.addEventListener('click', (event) => {
+    if(event.target.classList.contains("btnClose")) {
+        modal.classList.remove("visible");
+    }
+});
+
+modal.addEventListener('click', (event) => {
+    event.preventDefault();
+        if(event.target.classList.contains("btnOK")) {
+        const inputmodal = modal.querySelector(".inputTextModal");
+        const inputId = +inputmodal.id;
+        const cardIndexInData = data.findIndex((task) => task.id === inputId);
+        const el = data[cardIndexInData]            
+        data.splice(cardIndexInData, 1, {
+            ...el,
+            title: inputmodal.value
+        });
+        addCard();
+        modal.classList.remove("visible");
+        
     }
 });
